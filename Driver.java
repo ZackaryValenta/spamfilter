b2972e9bb284f0836f4226a4581db09b44703ba8
ZackaryValenta <zackary.valenta@gmail.com>
2014-10-27 01:02:41 -0400

    Driver update
    - removed demo code for FilteredDocument
    - improved demo code for SpamChecker to notify user of completion of
    creation and export

--- Driver.java
+++ Driver.java
@@ -10,23 +10,17 @@ public class Driver
 		File spamFolder = new File("./dataset/spam");
 		File[] hamFiles = hamFolder.listFiles();
 		File[] spamFiles = spamFolder.listFiles();
-		
+
+		System.out.print("Files in ham dataset: ");
 		System.out.println(hamFiles.length);
+		System.out.print("Files in spam dataset: ");
 		System.out.println(spamFiles.length);
-		System.out.println();
-		
-		FilteredDocument testDocument = new FilteredDocument(spamFiles[1].getAbsolutePath());
-		System.out.println(testDocument.getDocumentContents());
-		
-		System.out.println("\ntest document's filtered words:\n");
-		for (String s : testDocument.getFilteredWords())
-		{
-			System.out.println(s);
-		}
-		
+
+		System.out.print("\nCreating SpamChecker using documents in \"dataset/ham\" and \"dataset/spam\" folders...");
 		SpamChecker testChecker = new SpamChecker("./dataset/ham", "./dataset/spam");
-		
+		System.out.println("Done\n");		
+		System.out.print("Exporting spam checker's vocabulary to \"dataset/datasetWords.txt\"...");
 		testChecker.exportDatasetWordsToTextFile("./dataset/datasetWords.txt");
-		
+		System.out.println("Done");		
 	}
 }
