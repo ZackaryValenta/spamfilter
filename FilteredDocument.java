d5d614cdaf11d13f1a228f35e7ba3b8052b4aaee
ZackaryValenta <zackary.valenta@gmail.com>
2014-12-01 17:55:52 -0500

    Added control of removing XML tags

--- FilteredDocument.java
+++ FilteredDocument.java
@@ -25,6 +25,7 @@ public class FilteredDocument
 		}
 	}
 
+	private static final boolean REMOVE_XML_TAGS = false;
 	private String absolutePath;
 	private String documentContents;
 	private ArrayList<String> filteredWords;
@@ -34,7 +35,7 @@ public class FilteredDocument
 		this.absolutePath     = absolutePath;
 		this.documentContents = fileContentsToString(absolutePath);
 		String tagsRemoved    = removeXMLTags(this.documentContents);
-		this.filteredWords    = getFilteredWords(tagsRemoved);
+		this.filteredWords    = getFilteredWords((REMOVE_XML_TAGS) ? tagsRemoved : this.documentContents);		// based on the REMOVE_XML_TAGS variable, a different verison of the document is passed here
 	}
 
 	public String getAbsolutePath()
